76a929d15028a2cfb98c0eb513ec0ca8e54c4b78
Edson Blanco <lotad_lombre@hotmail.com>
2023-05-29 13:53:08 -0600

    feat:delete spec

--- curso/src/app/components/content-transfer-data/body.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BodyDirective } from "../directives/body.directive";
-
-describe('BodyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BodyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
